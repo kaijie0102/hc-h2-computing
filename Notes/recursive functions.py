ca26e05d7cd3790e5e846dde5fc231e3d98f7560
kaijie0102 <leowkaijie@gmail.com>
2018-05-14 11:42:40 +0800

    .

--- Notes/recursive functions.py
+++ Notes/recursive functions.py
@@ -1,3 +1,4 @@
+'''
 def power(x,y):
     if y == 1:
         return x
@@ -12,4 +13,23 @@ def main():
     y = int(input('Enter a positive integer:'))
     print(x,'to the power of',y,'is',power(x,y))
 main()
+
+
+total=0
+def Sum(lower,upper):
+    if lower <= upper:
+        return lower + Sum(lower+1,upper)
+    
+    else:
+        return 0
+'''       
+'''
+def main():
+    first = int(input('Enter the first integer: '))
+    
+    second = int(input('Enter the second integer: '))
+
+    print('The sum of the numbers from',first,'to',second,'is',Sum(first,second))
+main()       
+'''
         
