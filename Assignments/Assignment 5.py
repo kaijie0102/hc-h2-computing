8f4a6483f2a7349b3170b2a6706f5eaefff7052f
kaijie0102 <leowkaijie@gmail.com>
2018-05-21 10:18:29 +0800

    .

--- Assignments/Assignment 5.py
+++ Assignments/Assignment 5.py
@@ -1,2 +1,11 @@
 def multiplication(x,y):
-    x + multiplication()
+    if y>0:
+        return x + multiplication(x,y-1)
+    else:
+        return 0
+    
+def main():
+    x = int(input('Enter value of x'))
+    y = int(input('Enter value of y'))
+    print(multiplication(x,y))
+main()
